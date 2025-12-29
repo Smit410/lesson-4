34194c823554b34bb6daa4d44fc3e9dce3b2bbf0
Smit410 <smit.shah.chintan@gmail.com>
2025-12-29 15:29:35 +1300

    first commit

---
+++ activity1.py
@@ -0,0 +1,8 @@
+tree1= 82
+tree2= 67
+tree3= 57
+tree4= 99
+tree5= 34
+total= tree1+tree2+tree3+tree4+tree5
+average= total/5
+print("the averge is:", average)
---
+++ activity2.py
@@ -0,0 +1,8 @@
+amount= int(input("type an amount: "))
+hundred= amount//100
+fifty= (amount%100)//50
+ten= ((amount%100)%50)//10
+print(hundred)
+print(fifty)
+print(ten)
+print("yaaaaaaaaaa!")
