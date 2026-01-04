930e5c45ea0085421abc7207acb73c74540a5e13
Smit410 <smit.shah.chintan@gmail.com>
2026-01-04 15:15:02 +1300

    added activity3.py

---
+++ activity3.py
@@ -0,0 +1,8 @@
+marksmath= float(input("plz gimme ur marks 4 maths: "))
+markswriting= float(input("plz gimme ur marks 4 writing: "))
+marksPE= float(input("plz gimme ur marks 4 PE: "))
+markssocialstudies= float(input("plz gimme ur marks 4 social studies: "))
+marksreading= float(input("plz gimme ur marks 4 reading: "))
+total= marksmath + markswriting + marksPE + markssocialstudies + marksreading
+percentage=(total/500)*100
+print("ur total mark r: ", percentage,"% Good Job suuuuuuuuuper")
